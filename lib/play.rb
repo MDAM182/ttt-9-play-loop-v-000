4d64ac5aaf1f0cb81a8b40e70379824c5397c6e6
Michael Morris <mdam2345@gmail.com>
2018-04-23 03:11:30 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,16 +37,16 @@ end
 
 def play(board)
   counter = 1
-  loop do 
-  counter += 1
+  loop do
+  turn(board)
   puts "Iteration #{counter} of the loop"
-  if counter >= 10 
+  if counter >= 10
     break
   end
 end
   counter = 1
     while counter <= 9
-    
+
     turn(board)
     break
   end
